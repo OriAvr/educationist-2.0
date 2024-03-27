8f564be2f2053ec0e9b38a740a0f80fe6879b214
OriAvr <ori.avr123@gmail.com>
2024-03-28 00:42:04 +0200

    Terraform Cloud setup

--- environment/dev/compute/backend.tf
+++ environment/dev/compute/backend.tf
@@ -1,7 +1,16 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "educationist-remote-state"
     key    = "compute/terraform.tfstate"
     region = "eu-west-3"
+  }
+}*/
+
+terraform {
+  cloud {
+    organization = "Ori-Avraham"
+    workspaces {
+      name = "educationist"
+    }
   }
 }
--- environment/dev/data/backend.tf
+++ environment/dev/data/backend.tf
@@ -1,7 +1,18 @@
+/*
 terraform {
   backend "s3" {
     bucket = "educationist-remote-state"
     key    = "data/terraform.tfstate"
     region = "eu-west-3"
+  }
+}
+*/
+
+terraform {
+  cloud {
+    organization = "Ori-Avraham"
+    workspaces {
+      name = "educationist"
+    }
   }
 }
