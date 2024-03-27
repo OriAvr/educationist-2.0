586848eeb01c14676dbc4ae5fe59b9c7addab34c
OriAvr <ori.avr123@gmail.com>
2024-03-27 21:27:26 +0200

    variable handling

--- environment/dev/data/parameters.tf
+++ environment/dev/data/parameters.tf
@@ -1,17 +1,17 @@
 data "aws_ssm_parameter" "vpc_id" {
-  name = "${var.tags.Environment}/my_vpc/vpc_id"
+  name = "/${var.tags.Environment}/my_vpc/vpc_id"
 }
 
 data "aws_ssm_parameter" "vpc_cidr_block" {
-  name = "${var.tags.Environment}/my_vpc/vpc_cidr_block"
+  name = "/${var.tags.Environment}/my_vpc/vpc_cidr_block"
 }
 
 data "aws_ssm_parameter" "database_subnet_group" {
-  name = "${var.tags.Environment}/my_vpc/subnets/database_subnet_group"
+  name = "/${var.tags.Environment}/my_vpc/subnets/database_subnet_group"
 }
 
 
 data "aws_ssm_parameter" "vpc_default_security_group_id" {
-  name = "${var.tags.Environment}/my_vpc/subnets/vpc_default_security_group_id"
+  name = "/${var.tags.Environment}/my_vpc/subnets/vpc_default_security_group_id"
 }
 
