d9551d73995e8582cf3ec28c56ca5caba9e6ba0c
OriAvr <ori.avr123@gmail.com>
2024-03-31 11:57:12 +0300

    TFC setup

--- code/data/variables.tf
+++ code/data/variables.tf
@@ -1,65 +1,81 @@
 variable "identifier" {
   description = "The name of the RDS instance"
   type        = string
+  default     = "my-rds-db"
 }
 
 variable "db_engine" {
   description = "The database engine to use"
   type        = string
+  default     = "mysql"
 }
 
 variable "engine_version" {
   description = "The engine version to use"
   type        = string
+  default     = "8.0"
 }
 
 variable "family" {
   description = "The engine family"
   type        = string
+  default     = "mysql8.0"
 }
 
 variable "major_engine_version" {
   description = "The engine version to use"
   type        = string
+  default     = "8.0"
 }
 
 variable "instance_class" {
   description = "The instance type of the RDS instance"
   type        = string
+  default     = "db.t3.micro"
 }
 
 variable "allocated_storage" {
   description = "The allocated storage in gigabytes"
   type        = number
+  default     = 10
 }
 
 variable "max_allocated_storage" {
   description = "The maximum allocated storage in gigabytes"
   type        = number
+  default     = 50
 }
 
 variable "db_name" {
   description = "The DB name to create. If omitted, no database is created initially"
   type        = string
+  default     = "mydatabase"
 }
 
 variable "db_username" {
   description = "Username for the master DB user"
   type        = string
+  default     = "admin"
 }
 
 variable "db_port" {
   description = "The port on which the DB accepts connections"
   type        = number
+  default     = 3306
 }
 
 variable "multi_az" {
   description = "Whether to allow multi az deployment or not."
   type        = bool
+  default     = false
 }
 
 variable "tags" {
   description = "Tags for the VPC resource."
   type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
--- code/network/variables.tf
+++ code/network/variables.tf
@@ -1,59 +1,74 @@
 variable "name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
+  default     = "my-vpc"
 }
 
 variable "cidr" {
   description = "The IPv4 CIDR block for the VPC."
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "azs" {
   description = "A list of availability zones names or ids in the region"
   type        = list(string)
+  default     = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
 }
 
 variable "private_subnets" {
   description = "CIDR blocks for the private subnets."
   type        = list(string)
+  default     = ["10.0.1.0/24"]
 }
 
 variable "public_subnets" {
   description = "CIDR blocks for the public subnets."
   type        = list(string)
+  default     = ["10.0.101.0/24"]
 }
 
 variable "database_subnets" {
   description = "CIDR blocks for the database subnets."
   type        = list(string)
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "enable_nat_gateway" {
   description = "Should be true to enable NAT Gateway in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "single_nat_gateway" {
   description = "Should be true to allow only one NAT Gateway in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "one_nat_gateway_per_az" {
   description = "Should be false to allow only one NAT Gateway in the VPC"
   type        = bool
+  default     = false
 }
 
 variable "flow_logs_bucket_arn" {
   description = "S3 flow logs bucket ARN"
   type        = string
+  default     = ""
 }
 
 variable "s3_region" {
   description = "Region of the s3 to be used by the Gateway Endpoint."
   type        = string
+  default     = "eu-west-3"
 }
 
 variable "tags" {
   description = "Tags for the VPC resource."
   type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
